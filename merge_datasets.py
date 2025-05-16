0abdd6c0288d03f1b085b107625aed102f847879
SarahNasir54 <sarah.nasir540@gmail.com>
2025-05-16 13:11:40 +0500

    Completed Original and Reposts Datasets

--- merge_datasets.py
+++ merge_datasets.py
@@ -9,18 +9,18 @@ from tqdm import tqdm
 import traceback
 
 DATA_PATHS = {
-    "CED" : [r"D:\text datasets\text datasets\CED_Dataset", "zh", "society,disasters,health,politics,science", "Weibo"],
+    #"CED" : [r"D:\text datasets\text datasets\CED_Dataset", "zh", "society,disasters,health,politics,science", "Weibo"],
     #"FbMultiLingMisinfo": [r'D:\text datasets\text datasets\FbMultiLingMisinfo.csv', "en,it,de,es,fr,pt,id,nl,tl,el,af,hr,da", "politics", "Facebook"],
-    #"MediaEval15": [r"D:\text+image\text+image\mediaeval2015", "en,es,id,fr,no,pt,it,nl,ar", "society,disasters,health,politics,science,technology,entertainment,publicsafety", "Twitter"],
+    "MediaEval15": [r"D:\text+image\text+image\mediaeval2015", "en,es,id,fr,no,pt,it,nl,ar", "society,disasters,health,politics,science,technology,entertainment,publicsafety", "Twitter"],
     #"Pheme5": [r'D:\text datasets\text datasets\phemernrdataset\pheme-rnr-dataset', "en","disaster,crime,publicsafety,religion", "Twitter"],
-    #"Pheme9": [r'D:\text+image\text+image\pheme9\pheme-rumour-scheme-dataset', "en,de", "disaster,crime,publicsafety,religion", "Twitter"],
-    #"Pheme-veracity": [r'D:\text datasets\text datasets\PHEME_veracity\all-rnr-annotated-threads', "en", "disaster,crime,publicsafety,religion", "Twitter"],
-    #"RumorEval17": [r"D:\text datasets\text datasets\RumorEval17\semeval2017-task8-dataset", "en", "others", "Twitter"],
-    #"RumorEval19": [r"D:\text datasets\text datasets\rumoureval2019\rumoureval2019\rumoureval-2019-training-data\rumoureval-2019-training-data", "en", "others", "Twitter,Reddit"],
-    #"Social-Honeypot": [r'D:\text datasets\text datasets\Social-Honeypot\social_honeypot_icwsm_2011', "en,es,ms,pt", "spam,politics,jobs,social", "Twitter"],
-    #"Twitter": [r'D:\text+image\text+image\twitter', "en,es,id,fr,no,pt,it,nl,ar", "society,disasters,health,politics,science,technology,entertainment,publicsafety", "Twitter"],
+    "Pheme9": [r'D:\text+image\text+image\pheme9\pheme-rumour-scheme-dataset', "en,de", "disaster,crime,publicsafety,religion", "Twitter"],
+    # "Pheme-veracity": [r'D:\text datasets\text datasets\PHEME_veracity\all-rnr-annotated-threads', "en", "disaster,crime,publicsafety,religion", "Twitter"],
+    # "RumorEval17": [r"D:\text datasets\text datasets\RumorEval17\semeval2017-task8-dataset", "en", "others", "Twitter"],
+    # "RumorEval19": [r"D:\text datasets\text datasets\rumoureval2019\rumoureval2019\rumoureval-2019-training-data\rumoureval-2019-training-data", "en", "others", "Twitter,Reddit"],
+    # "Social-Honeypot": [r'D:\text datasets\text datasets\Social-Honeypot\social_honeypot_icwsm_2011', "en,es,ms,pt", "spam,politics,jobs,social", "Twitter"],
+    # "Twitter": [r'D:\text+image\text+image\twitter', "en,es,id,fr,no,pt,it,nl,ar", "society,disasters,health,politics,science,technology,entertainment,publicsafety", "Twitter"],
     #"Weibo-data": [r'D:\text+image\text+image\Weibo-dataset-main\Weibo-dataset-main', "zh", "society,disasters,health,politics,science,technology,entertainment,publicsafety", "Weibo"],
-    "Weibo-Rumor": [r"D:\text datasets\text datasets\weibo rumor", "zh", "politics", "Weibo"],
+    #"Weibo-Rumor": [r"D:\text datasets\text datasets\weibo rumor", "zh", "politics", "Weibo"],
 }
 
 # fill this dictionary for only special cases
@@ -40,33 +40,60 @@ convert_to_df = {
 
 convert_to_reposts = {
     "CED": load_ced_repost_posts,
+    "Pheme5": load_pheme5_reposts,
+    "Pheme9": load_pheme9_reposts,
+    "Pheme-veracity": load_phemeveracity_reposts,
+    "RumorEval17": load_rumoureval17_reposts,
+    "RumorEval19": load_rumoureval2019_reposts,
+    "Weibo-Rumor": load_weibo_rumor_reposts
 }
 
 def update_fields(dataset_name, df):
     required_columns = [
         "post_id", "text", "timestamp", "label", "username",
         "follower_count", "friends_count", "is_verified",
-        "repost_text", "repost_count", "likes", "language", "domain", "platform"
+        "repost_count", "likes", "language", "domain", "platform"
     ]
     
     # Check for missing columns and add them with default values
     for col in required_columns:
         if col not in df.columns:
             # Set default values based on the column type
-            if col in ["username", "text", "repost_text", "timestamp", "is_verified"]:
+            if col in ["username", "text", "timestamp", "is_verified"]:
                 df[col] = ""
             else:
                 df[col] = 0
-            if col == "label":
-                df[col] = "unknown"
-
     return df
 
+def update_reposts_fields(dataset_name, df):
+    required_columns = ["post_id", "repost_text", "label"]
+    
+    for col in required_columns:
+        if col not in df.columns:
+            if col in ["post_id", "repost_text"]:
+                df[col] = ""
+    return df
+
+def process_and_map_reposts(dataset_name, df, mapping):
+    repost_mapping = mapping.get("repost_mapping", {})
+
+    df = update_reposts_fields(dataset_name, df)
+
+    df = df[[repost_mapping["post_id"], repost_mapping["repost_text"], repost_mapping["label"]]]
+
+    df = df.rename(columns={
+        repost_mapping["post_id"]: "post_id",
+        repost_mapping["repost_text"]: "repost_text",
+        repost_mapping["label"]: "label",
+    })
+
+    df["repost_text"] = df["repost_text"].astype(str).apply(clean_text)
+
+    return df[["post_id", "repost_text", "label"]]
 
 def process_and_map(dataset_name, df, mapping):
     column_mapping = mapping.get("column_mapping", {})
     label_mapping = mapping.get("label_mapping", {})
-    repost_mapping = mapping.get("repost_mapping", {})
 
     df = update_fields(dataset_name, df)
 
@@ -86,14 +113,6 @@ def process_and_map(dataset_name, df, mapping):
         column_mapping["likes"]: "likes"
     })
 
-    # df = df[[repost_mapping["post_id"], repost_mapping["repost_text"], repost_mapping["label"]]]
-
-    # df = df.rename(columns={
-    #     repost_mapping["post_id"]: "post_id",
-    #     repost_mapping["repost_text"]: "repost_text",
-    #     repost_mapping["label"]: "label",
-    # })
-
     # Map labels to standard labels
     labels_to_keep = list(label_mapping.keys())
     
@@ -106,7 +125,6 @@ def process_and_map(dataset_name, df, mapping):
 
     # Clean text fields
     df["text"] = df["text"].astype(str).apply(clean_text)
-    #df["repost_text"] = df["repost_text"].astype(str).apply(clean_text)
 
     df["is_verified"] = df["is_verified"].astype(bool)
 
@@ -120,7 +138,7 @@ def process_and_map(dataset_name, df, mapping):
     df["platform"] = DATA_PATHS[dataset_name][3]
 
 
-    return df[["post_id", "text", "timestamp", "label", "username", "follower_count", "friends_count", "is_verified", "repost_count", "likes", "language", "domain", "platform"]] #, df[["post_id", "repost_text", "label"]]
+    return df[["post_id", "text", "timestamp", "label", "username", "follower_count", "friends_count", "is_verified", "repost_count", "likes", "language", "domain", "platform"]] 
 
 
 
@@ -136,9 +154,17 @@ def main():
             df = convert_to_df[dataset_name](path)
         else:
             df = load_dataset(path)  
+        original_posts = process_and_map(dataset_name, df, mappings[dataset_name])
 
-        return process_and_map(dataset_name, df, mappings[dataset_name])
+        reposts_df = None
+        reposts = None
+        
+        if dataset_name in convert_to_reposts.keys():
+            reposts_df = convert_to_reposts[dataset_name](path)
+            reposts = process_and_map_reposts(dataset_name, reposts_df, mappings[dataset_name])
 
+        return original_posts , reposts 
+    
     # Use ThreadPoolExecutor to process datasets concurrently
     with ThreadPoolExecutor(max_workers=25) as executor:
         futures = {
@@ -149,9 +175,9 @@ def main():
         # Show progress with tqdm
         for future in tqdm(as_completed(futures), total=len(futures), desc="Processing datasets"):
             try:
-                originals = future.result()
+                originals, repost = future.result()
                 originals_list.append(originals)
-                #reposts_list.append(reposts)
+                reposts_list.append(repost)
             except Exception as e:
                 name = futures[future]
                 print(f"Error processing {name}: {e}")
@@ -160,10 +186,10 @@ def main():
     # Save the final merged datasets
     os.makedirs("processed_data", exist_ok=True)
     all_originals = pd.concat(originals_list, ignore_index=True)
-    #all_reposts = pd.concat(reposts_list, ignore_index=True)
+    all_reposts = pd.concat(reposts_list, ignore_index=True)
     save_dataset(all_originals, "processed_data/all_originals.parquet")
-    #save_dataset(all_reposts, "processed_data/all_reposts.parquet") | Reposts: {len(all_reposts)}
-    print(f"Merged Social Media dataset saved with {len(all_originals)} records.") 
+    save_dataset(all_reposts, "processed_data/all_reposts.parquet")
+    print(f"Merged Social Media dataset saved with {len(all_originals)} records and reposts are {len(all_reposts)}")
 
 # Run the main function if this script is executed
 if __name__ == '__main__':
